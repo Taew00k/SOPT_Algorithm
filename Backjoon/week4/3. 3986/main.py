2fdb7add86f37b1b4f9ce026f740e7225e3b01af
Taew00k <davis0625@dgu.ac.kr>
2024-11-21 18:17:52 +0900

    4375 구현

---
+++ Backjoon/week4/3. 3986/main.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+# 입력
+n = int(input())
+cnt = n
+for _ in range(n):
+    text = input().rstrip()
+    stack = []
+
+    for s in text:
+        if stack and stack[-1] == s:
+            stack.pop()
+        else:
+            stack.append(s)
+
+    if stack:
+        cnt -= 1
+print(cnt)
